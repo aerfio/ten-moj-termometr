8d1a36ffc287a9172c7688246ba72faa46bf69be
aerfio <mati6095@gmail.com>
2017-04-07 21:10:55 +0200

    first commit

---
+++ Debug/ten moj termomeetr.cpp.d
@@ -0,0 +1,20 @@
+C:\Users\Aerfio\AppData\Local\Temp\VMBuilds\ten\ moj\ termomeetr\uno\Debug\ten\ moj\ termomeetr.cpp.o: \
+ C:\Users\Aerfio\AppData\Local\Temp\VMBuilds\ten\ moj\ termomeetr\uno\Debug\ten\ moj\ termomeetr.cpp \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/arduino.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/binary.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/WCharacter.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/WString.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/HardwareSerial.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Stream.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Print.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Printable.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/USBAPI.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Arduino.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\variants\standard/pins_arduino.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\libraries\EEPROM\src/EEPROM.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\LiquidCrystal\src/LiquidCrystal.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Print.h \
+ C:\Users\Aerfio\Documents\Arduino\libraries\OneWire-master/OneWire.h \
+ C:\Users\Aerfio\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.18\cores\arduino/Arduino.h \
+ C:\Users\Aerfio\Documents\Arduino\libraries\Arduino-Temperature-Control-Library-master/DallasTemperature.h \
+ E:\Visual\ Studio\Common7\IDE\Extensions\xsnkzden.gvu\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
